4d0248619cf3f3cf6784f5b9026edcf8613ad430
Carlo Dino <cdino@stanford.edu>
2023-12-28 23:33:18 -0800

    Updated dependencies, started initial styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `carlo-dino`,
+    title: `carlodino`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
@@ -20,21 +20,5 @@ module.exports = {
       "path": "./src/pages/"
     },
     __key: "pages"
-  }, {
-    resolve: `gatsby-omni-font-loader`,
-    options: {
-      enableListener: true,
-      preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
-      web: [
-        {
-          name: `Open Sans`,
-          file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap`,
-        },
-        {
-          name: 'Raleway',
-          file: 'https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap'
-        }
-      ],
-    },
   }]
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // transparent: 'transparent',
+        // black: '#000',
+        // white: '#fff',
+        // gray: {
+        //   100: '#f7fafc',
+        //   900: '#1a202c',
+        // },
+        customPurpleBG: '#0f021a',
+      }
+    },
   },
   plugins: [],
+  safelist: [{
+    pattern: /(bg|text)/
+  }]
 }
+
+
