9407de914693d6ae020f63f1ad8ae8282c881645
Carlo Dino <cdino@stanford.edu>
2024-01-04 18:41:49 -0800

    Added text animation keyframes
    Added text animation keyframes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,22 @@ module.exports = {
         //   900: '#1a202c',
         // },
         customPurpleBG: '#0f021a',
-      }
+      },
+      animation: {
+            text:'text 5s ease infinite',
+        },
+        keyframes: {
+            text: {
+                '0%, 100%': {
+                    'background-size':'200% 200%',
+                    'background-position': 'left center'
+                },
+                '50%': {
+                    'background-size':'200% 200%',
+                    'background-position': 'right center'
+                }
+            },
+        }
     },
   },
   plugins: [],
